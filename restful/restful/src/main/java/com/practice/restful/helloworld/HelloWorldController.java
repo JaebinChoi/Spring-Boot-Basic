9b477ce3fdfbc43e42901696a68c77092fdd1744
JaebinChoi <choijaebin13@gmail.com>
2020-08-18 12:18:48 +0900

    Validation, Internationlization => Not Suitable Success

--- restful/restful/src/main/java/com/practice/restful/helloworld/HelloWorldController.java
+++ restful/restful/src/main/java/com/practice/restful/helloworld/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.practice.restful.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+    @Autowired
+    private MessageSource messageSource;
+
     // HTTP 통신 방식 : GET
     // Mapping URI : /hello-world (endpoint)
     // @RequestMapping(method=RequestMethod.GET, path="/hello-world")
@@ -25,5 +33,10 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
         System.out.println(name);
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public  String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("greeting.message", null, locale);
     }
 }
